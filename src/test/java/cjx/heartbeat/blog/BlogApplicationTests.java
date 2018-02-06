2dd6ecc3333635aaf52581a6804f75113d6d4b28
chenjunxu <chenjunxu@kingyea.com.cn>
2018-02-06 16:19:04 +0800

    test

--- src/test/java/cjx/heartbeat/blog/BlogApplicationTests.java
+++ src/test/java/cjx/heartbeat/blog/BlogApplicationTests.java
@@ -1,16 +1,29 @@
 package cjx.heartbeat.blog;
 
+import cjx.heartbeat.blog.management.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class BlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void testtest(){
+		System.out.println(userService.checkUsername("lobinson"));
 	}
 
 }
